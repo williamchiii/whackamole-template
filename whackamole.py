170bbca6ce2a64f2972374d4fcf050f969312647
William Chi <wchi@ufl.edu>
2024-11-16 01:40:55 -0500

    Added X axis Lines and initial mole position

--- whackamole.py
+++ whackamole.py
@@ -6,10 +6,11 @@ def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+
         running = True
         while running:
             for event in pygame.event.get():
@@ -18,11 +19,19 @@ def main():
             screen.fill("light green")
             #Draw Y Axis Lines Beginning
             draw_y = 512 // 32
-            x = 0
+            y = 0
             for i in range(draw_y):
-                x += 32
-                pygame.draw.line(screen, 'black', (0, x), (640, x))
+                y += 32
+                pygame.draw.line(screen, 'black', (0, y), (640, y))
             #Draw Y Axis Lines End
+            #Draw X Axis Lines Begin
+            draw_x = 640//32
+            x = 0
+            for i in range(draw_x):
+                x += 32
+                pygame.draw.line(screen, 'black',(x, 0), (x,512))
+            #Draw X Axis Lines End
+            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0))) #Initial Pos
             pygame.display.flip()
             clock.tick(60)
     finally:
